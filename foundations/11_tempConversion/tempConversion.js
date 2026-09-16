dc30a3c4fc25b4dc220be62a1d13b8a67d1fafe4
Omar Abu Gali <omarsamirghali@gmail.com>
2026-09-16 18:59:34 +0300

    finish exercise 11-tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  // formula
+  // x °F = (x - 32) * 5/9 °C
+  let celsius = (x - 32) * 5/9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  // formula
+  // x °C = (x * 9/5 + 32) °F
+  let fahrenheit = (x * 9 / 5 + 32)
+  return Math.round(fahrenheit * 10) / 10;
 };
+
+// console.log(convertToFahrenheit(0));
+// console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
