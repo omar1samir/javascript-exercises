e3de1c6dfe624dbfa51623b458a00c24eb72c1e9
Omar Abu Gali <omarsamirghali@gmail.com>
2026-09-16 12:09:46 +0300

    finish exercise 07-reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = '';
 
+    for (let i = str.length - 1; i >= 0; i--){
+        reversed += str[i];
+    }
+    return reversed;
 };
+
+// console.log(reverseString("hello"));
+// console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
