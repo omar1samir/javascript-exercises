57ec5420a707ec98cf1716f084b1f5dc3cd52778
Omar Abu Gali <omarsamirghali@gmail.com>
2026-09-16 17:49:07 +0300

    finish exercise 09-sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // if the inputs are not positive integers return 'ERROR'
+    if (typeof(a) !== 'number' || !Number.isInteger(a) || a <= 0
+    || typeof(b) !== 'number' || !Number.isInteger(b) || b <= 0)
+        return 'ERROR'
+    
 
+    // find min and max
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+
+    // loop from min to max 
+    let sum = 0;
+    for (let i = min; i <= max; i++)
+        // sum all numbers on the way
+        sum += i;
+    
+
+    // return the sum 
+    return sum
 };
+
+// console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
