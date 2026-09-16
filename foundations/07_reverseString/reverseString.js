57e047cb6db25b2a6dfa24124256e39d50e4a2ab
Omar Abu Gali <omarsamirghali@gmail.com>
2026-09-16 12:11:41 +0300

    made some syntax enhancements

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,9 +1,9 @@
 const reverseString = function(str) {
+    
     let reversed = '';
 
-    for (let i = str.length - 1; i >= 0; i--){
-        reversed += str[i];
-    }
+    for (let i = str.length - 1; i >= 0; i--) reversed += str[i];
+
     return reversed;
 };
 
