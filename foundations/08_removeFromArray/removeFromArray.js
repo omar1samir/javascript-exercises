b455124298ed4cc9a8c337829fccceae9a7496ef
Omar Abu Gali <omarsamirghali@gmail.com>
2026-09-16 13:39:59 +0300

    finish exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deleteArgs) {
+    // loop through the arr
+    for (let i = 0; i < deleteArgs.length; i++){
+        // filter the 
+        return arr.filter(element => !deleteArgs.includes(element));
+    } 
 };
+
+// console.log(removeFromArray([1, 2, 3, 4, 5, 6], 3, 6));
 
 // Do not edit below this line
 module.exports = removeFromArray;
